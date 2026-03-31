6cc69e8a3de44fb16fe452a0bdd17102d02384db
AEBaq <114154989+AEBaq@users.noreply.github.com>
2026-03-31 20:53:01 +0200

    Add header comment to optimized_by_student.c
    Added a header comment with metadata and context for the optimized code.

--- Lab03/code/example_1/optimized_by_student.c
+++ Lab03/code/example_1/optimized_by_student.c
@@ -1,1 +1,24 @@
-
+/*****************************************************************************************
+ * HEIG-VD
+ * Haute Ecole d'Ingenerie et de Gestion du Canton de Vaud
+ * School of Business and Engineering in Canton de Vaud
+ *****************************************************************************************
+ * REDS Institute
+ * Reconfigurable Embedded Digital Systems
+ *****************************************************************************************
+ *
+ * File                 : optimized_by_student.c
+ * Author               : AEBaq
+ * Date                 : 31 mars 2026
+ *
+ * Context              : HPC - Lab03
+ *
+ *****************************************************************************************
+ * Brief: This is the code opitmized by student for the first example
+ *
+ *****************************************************************************************
+ * Modifications :
+ * Ver    Date        Student      Comments
+ * 1.0    31.03.2026  AEBaq        First version of opitmized code by student
+ *
+*****************************************************************************************/
