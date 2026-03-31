72ef1bdce676adaa3dd08b34a1df9817da0e279e
AEBaq <114154989+AEBaq@users.noreply.github.com>
2026-03-31 21:07:00 +0200

    Optimize loop by calculating constant outside
    Optimized the loop by moving constant expression calculation outside.

--- Lab03/code/example_1/optimized_by_student.c
+++ Lab03/code/example_1/optimized_by_student.c
@@ -22,3 +22,18 @@
  * 1.0    31.03.2026  AEBaq        First version of opitmized code by student
  *
 *****************************************************************************************/
+#include <stdio.h>
+
+void constant_expression_outside_loop() {
+    int *a = {0, 1, 2, 3};
+    int * b = {4, 5, 6, 7};
+    int n = 4, i, constant = 2 * 3;
+    for (i = 0; i < n; i++) {
+        a[i] = b[i] * constant;  // Calcul fait une seule fois
+    }
+}
+
+int main() {
+    constant_expression_outside_loop();
+    return 0;
+}
