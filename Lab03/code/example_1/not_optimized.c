348e42f0f81c56ce95f3fac3d5a2042e7219eb6c
AEBaq <114154989+AEBaq@users.noreply.github.com>
2026-03-31 22:38:40 +0200

    Update not_optimized.c

--- Lab03/code/example_1/not_optimized.c
+++ Lab03/code/example_1/not_optimized.c
@@ -30,7 +30,7 @@ void constant_expression_inside_loop() {
     int * b = {4, 5, 6, 7};
     int n = 4, i;
     for (i = 0; i < n; i++) {
-        a[i] = b[i] * 2 * 3;  // 2*3 calculé à chaque itération, pas optimisé
+        a[i] = b[i] * 2 * 3;
     }
 }
 
