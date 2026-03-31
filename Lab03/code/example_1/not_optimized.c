433c31f08526319b6e12f93394a58480fc53a6ae
AEBaq <114154989+AEBaq@users.noreply.github.com>
2026-03-31 20:50:06 +0200

    Add initial unoptimized code for Lab03 example
    Initial version of the unoptimized code for Lab03 example.

---
+++ Lab03/code/example_1/not_optimized.c
@@ -0,0 +1,24 @@
+/*****************************************************************************************
+ * HEIG-VD
+ * Haute Ecole d'Ingenerie et de Gestion du Canton de Vaud
+ * School of Business and Engineering in Canton de Vaud
+ *****************************************************************************************
+ * REDS Institute
+ * Reconfigurable Embedded Digital Systems
+ *****************************************************************************************
+ *
+ * File                 : not_optimized.c
+ * Author               : AEBaq
+ * Date                 : 31 mars 2026
+ *
+ * Context              : HPC - Lab03
+ *
+ *****************************************************************************************
+ * Brief: This is the code not opitmized for the first example
+ *
+ *****************************************************************************************
+ * Modifications :
+ * Ver    Date        Student      Comments
+ * 1.0    31.03.2026  AEBaq        First version of not opitmized code
+ *
+*****************************************************************************************/
